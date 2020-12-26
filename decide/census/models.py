767ad6bc34469de4e10212bf8130d8db5dbeff91
maria <marcascal2@alum.us.es>
2020-12-26 19:26:36 +0100

    añado el atributo adscripcion a la clase censo

--- decide/census/models.py
+++ decide/census/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-
+    adscripcion = models.CharField(max_length=20, blank=True, null=True)
     class Meta:
-        unique_together = (('voting_id', 'voter_id'),)
+        unique_together = (('voting_id', 'voter_id', 'adscripcion'),)
