b6af282a21d19a45e7d8134ddb0dfb96a20878ee
Moisés Calzado <moisescalzado1227@gmail.com>
2020-12-30 15:21:19 +0100

    (Desarrollo)Añado el modelo UserData
    Agrego un nuevo modelo que almacene los datos de un usuario, en esta caso tan solo almacenamos la edad

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
@@ -7,3 +7,15 @@ class Census(models.Model):
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+
+
+class UserData(models.Model):
+    age = models.PositiveIntegerField()
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return 'Datos del usuario : ' + self.user.username
