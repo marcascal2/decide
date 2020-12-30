99bf1733e30c8dba987e7fefe6e927c7b70f1213
GermanMT <germanoctako.gmail.com>
2020-12-30 18:45:51 +0100

    (Funcionalidad)Implementacion de la exportación
    He añadido un metodo capaz de exportar el número de censos seleccionados a un archivo .csv.

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
-from import_export.admin import ImportExportActionModelAdmin
+from django.http import HttpResponse
+import csv
 
 from .models import Census
 
 
-class CensusAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
+class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')
     list_filter = ('voting_id', )
 
     search_fields = ('voter_id', )
 
+    actions = ['export_as_csv','import_from_csv']
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+
+    export_as_csv.short_description = 'Exportar a CSV'
 
 admin.site.register(Census, CensusAdmin)
