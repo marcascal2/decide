8e18a7c2f44a208a269a542601c101a9bc352b42
Amine Chaghir Chikhaoui <zopizopizala@hotmail.es>
2020-12-31 10:16:39 +0100

    (Implementacion)Un encabezado
    Cuerpo del commit
    
    Desarrollo de la lectura de datos del candidato mediante el JSON

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -15,7 +15,27 @@ class PostProcView(APIView):
 
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
-
+    
+    def paridad (self,options):
+        out = []
+        
+        for opt in options:
+            out.append({
+                **opt,
+                'paridad': [],
+                })
+            
+        for indice in out:
+            candidatos = indice['dandidates']
+            hombre = []
+            mujeres = []
+            for cand in candidatos:
+                if cand['sex'] == 'H':
+                    hombre.append(cand)
+                elif cand['sex'] == 'M':
+                    mujeres.append(cand)
+                    
+    
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -31,7 +51,8 @@ class PostProcView(APIView):
 
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
-
+        cands = request.data.get('candidates', [])
+        print(cands)
         if t == 'IDENTITY':
             return self.identity(opts)
 
