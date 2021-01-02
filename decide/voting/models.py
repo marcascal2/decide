462b49b78c588d84f7b6be971238a48f63f4a6d6
ChaoHu99 <chahu@alum.us.es>
2021-01-02 14:28:11 +0100

    (Incidencia) Incidencia#64
    Al comprobar una restricción en el save, este lo comprueba siempre .Por lo que,
    no se podía crear una votación sin min_age y max_age. Ya está arreglado.

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -50,10 +50,12 @@ class Voting(models.Model):
     postproc = JSONField(blank=True, null=True)
 
     def save(self):
-        if self.min_age is None or self.max_age is none:
+        if self.min_age is None or self.max_age is None:
             return super().save()
-        if self.min_age > self.max_age :
+        elif self.min_age > self.max_age :
             raise ValidationError("Edad máxima debe ser mayor que la edad mínima")
+            return super().save()
+        else: 
             return super().save()
 
     def create_pubkey(self):
