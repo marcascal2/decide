1177fd0d69a9b0fd42aef1dfd936223bf0c25b0a
ferangmor <ferangmor@alum.us.es>
2021-01-03 12:58:56 +0100

    (Desarrollo) Añado estilos al frontend de cenus
    Añadido el estilo al frontend en el apartado de census

--- decide/census/views.py
+++ decide/census/views.py
@@ -1,6 +1,7 @@
 from django.db.utils import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.status import (
         HTTP_201_CREATED as ST_201,
@@ -49,3 +50,6 @@ class CensusDetail(generics.RetrieveDestroyAPIView):
         except ObjectDoesNotExist:
             return Response('Invalid voter', status=ST_401)
         return Response('Valid voter')
+
+def adminView(request):
+    return render(request,'admin.html')
