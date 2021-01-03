5f43da2440ddb85d31ab7174f5d9e891d3028f81
albcorare <albcoare@alum.us.es>
2021-01-03 18:47:34 +0100

    (test)Ley Simple
    Test modificados

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -48,25 +48,25 @@ class PostProcTestCase(APITestCase):
     def test_simple(self):
         data = {
             'type': 'SIMPLE',
-            'seats':7,
+            'seats': 7,
             'options': [
                 { 'option': 'Option 1', 'number': 1, 'votes': 5 }, 
                 { 'option': 'Option 2', 'number': 2, 'votes': 2 },
                 { 'option': 'Option 3', 'number': 3, 'votes': 5 },
-                { 'option': 'Option 4', 'number': 4, 'votes': 0 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
                 { 'option': 'Option 5', 'number': 5, 'votes': 5 },
-                { 'option': 'Option 6', 'number': 6, 'votes': 0 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
         
             ]
         }
 
         expected_result = [
             { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 2 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 5, 'postproc': 2 },
             { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 2 },
-            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 1 },
-            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 1 },
-            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },
-            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 2, 'postproc': 1 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 0 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 0 },
         ]
 
         response = self.client.post('/postproc/', data, format='json')
@@ -80,22 +80,22 @@ class PostProcTestCase(APITestCase):
             'type': 'SIMPLE',
             'seats':40,
             'options': [
-                { 'option': 'Option 1', 'number': 1, 'votes': 4 },
-                { 'option': 'Option 2', 'number': 2, 'votes': 3 },
-                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
+                { 'option': 'Option 1', 'number': 1, 'votes': 10 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 8 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 1 },
                 { 'option': 'Option 4', 'number': 4, 'votes': 2 },
-                { 'option': 'Option 5', 'number': 5, 'votes': 3 },
-                { 'option': 'Option 6', 'number': 6, 'votes': 0 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 4 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 5 },
             ]
         }
 
         expected_result = [
-            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 13 },
-            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 13 },
-            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 7 },
-            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 5 },
-            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 2 },
-            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+            { 'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 13 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 8, 'postproc': 11 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 5, 'postproc': 7 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 4, 'postproc': 5 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 3 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 1, 'postproc': 1 },
         ]
 
         response = self.client.post('/postproc/', data, format='json')
@@ -103,3 +103,4 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
