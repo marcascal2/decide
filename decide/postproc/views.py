a4c9e2c9af8d93b6516a1740c06403600b611820
serpercap <serpercap@alum.us.es>
2021-01-03 20:34:14 +0100

    Realizo incremento en Sainte Lague
    Realizo el metodo y lo dejo preparado para implementar su versión modificada.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -15,9 +15,48 @@ class PostProcView(APIView):
 
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
-    def saintelague(self, options, numeroEscanos):
-
+    
+    def saintelague(self, options, nSeats):
         
+        #Definimos las variables
+
+        partidos = [] 
+        puntosPorPart = [] 
+        escanos = [] 
+        out = [] 
+
+       #Ponemos los escaños iniciales de todos los partidos a 0
+        for n in options:
+            escanosIniciales = 0
+            escanos.append(escanosIniciales)
+
+        #Añadimos los votos a cada partido y a out todas las salidas anterioes mas los escaños
+        for opt in options:
+            partidos.append(opt['votes']) 
+            out.append({
+                **opt,
+                'seats': 0,
+                })
+
+        #Inicializamos la lista así para que no se cambie por referencia
+        puntosPorPart = partidos[:]
+        escanosTotales = nSeats 
+        i = 0
+        while(i<escanosTotales):
+            maxVotos = max(puntosPorPart) 
+            index = puntosPorPart.index(maxVotos)
+            if maxVotos != 0:
+                
+                escanos[index] += 1 
+                out[index]['seats'] += 1 
+                puntosPorPart[index] = partidos[index] / ((2*escanos[index])+1) 
+
+            i=i+1
+            
+        out.sort(key=lambda x: -x['seats'])
+        return Response(out)
+
+        #Me faltaria hacer el saintelague modificado
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -36,5 +75,7 @@ class PostProcView(APIView):
 
         if t == 'IDENTITY':
             return self.identity(opts)
+        elif t == 'SAINTELAGUE':
+            return self.saintelague(opts,request.data.get('nSeats'))
 
         return Response({})
