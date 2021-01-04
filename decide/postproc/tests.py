0bc4b04bb31f2ae733c53b6efd6f5612c774b409
antcorlav <antcorlav@alum.us.es>
2021-01-04 20:02:33 +0100

    (test)Metodo de ganador unico
    Nuevo test que comprueba el funcionamiento del metodo cuando hay dos ganadores empatados en votos y no se puede hacer un reparto equitativo de seats

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -101,3 +101,32 @@ class PostProcTestCase(APITestCase):
         values = response.json()
         self.assertEqual(values, expected_result)
 
+    def test_mgu3(self):
+        data = {
+            'type': 'MGU',
+            'seats': 32,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 6 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 5 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 15 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 15 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 15 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 9 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 3', 'number': 3, 'votes': 15, 'postproc': 10 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 15 , 'postproc': 10 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 15, 'postproc': 10 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 9, 'postproc': 2 },
+            { 'option': 'Option 1', 'number': 1, 'votes': 6, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 5, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
