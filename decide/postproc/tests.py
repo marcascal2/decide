d1f8e009de058e767d272ec3afe38c679ea08171
antcorlav <antcorlav@alum.us.es>
2021-01-05 11:45:58 +0100

    (test)Metodo de ganador unico
    Añadido dos test nuevos, uno comprueba el funcionamiento del metodo cuando hay mas ganadores empatados que seats a repartir y el otro es una version distinta de la situacion con varios ganadores sin un reparto equitativo de seats

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -159,3 +159,60 @@ class PostProcTestCase(APITestCase):
         values = response.json()
         self.assertEqual(values, expected_result)
 
+    def test_mgu5(self):
+        data = {
+            'type': 'MGU',
+            'seats': 5,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 8 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 8 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 8 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 8 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 8 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 8 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 8, 'postproc': 1 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 8 , 'postproc': 1 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 8, 'postproc': 1 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 8, 'postproc': 1 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 8, 'postproc': 1 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 8, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_mgu6(self):
+        data = {
+            'type': 'MGU',
+            'seats': 43,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 6 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 5 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 15 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 15 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 15 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 15 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 3', 'number': 3, 'votes': 15, 'postproc': 10 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 15 , 'postproc': 10 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 15, 'postproc': 10 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 15, 'postproc': 10 },
+            { 'option': 'Option 1', 'number': 1, 'votes': 6, 'postproc': 3 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 5, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
