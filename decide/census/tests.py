727fbe73cd6bb3a11e0edaaf14d6927ebeb20d3c
emicolalc <emicolalc@alum.us.es>
2021-01-11 20:11:55 +0100

    (Testing)Test agrupacion y filtro
    Implementados test de agrupaciones y filtro

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -16,6 +16,17 @@ from django.urls import reverse
 import io
 import csv
 
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+from base.tests import BaseTestCase
+
 class CensusTestCase(BaseTestCase):
 
     def setUp(self):
@@ -300,4 +311,81 @@ class CensusTestCase(BaseTestCase):
             cadena2 = row2.decode('utf-8')
             self.assertEqual(cadena1, cadena2)
         csv.close()
+
+class AdminTestCase(StaticLiveServerTestCase):
+
+    def setUp(self):
+        #Load base test functionality for decide
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        self.driver = webdriver.Chrome(options=options)
+
+        super().setUp() 
+        question = Question(desc='desc')
+        question.save()
+
+        v1 = Voting(id = 1, name='voting_testing1', question=question)
+        v1.save()
         
+        v2 = Voting(id = 3, name='voting_testing2', question=question)
+        v2.save()
+
+        user1 = User(id=5, username='voter1', password='test_password')
+        user1.save()
+        
+        user2 = User(id=6, username='voter2', password='test_password')
+        user2.save()
+
+        census = Census(id=21, voting_id=1, voter_id=5, adscripcion='Colegio1', date=date.today())
+        census.save()
+
+        c2 = Census(id=22,voting_id=3, voter_id=5, adscripcion='Colegio1', date=date.today())
+        c2.save()
+
+        c3 = Census(id=23, voting_id=1, voter_id=6, adscripcion='Colegio2', date=date.today())
+        c3.save()           
+            
+    def tearDown(self):           
+        super().tearDown()
+        self.driver.quit()
+
+        self.base.tearDown()
+
+    def grouping_by_voter_test(self):
+        self.driver.get(f'{self.live_server_url}/admin/')
+        self.driver.find_element_by_id('id_username').send_keys("admin")
+        self.driver.find_element_by_id('id_password').send_keys("qwerty",Keys.ENTER)
+
+        self.driver.get(f'{self.live_server_url}/census/admin/')
+        self.driver.find_element(By.ID, "agrupar_votante").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#headingThree > .accordion-button").click()
+        self.driver.find_element(By.LINK_TEXT, "voter1").click()
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(1) > td:nth-child(2)").text == "voter1"
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(2) > td:nth-child(2)").text == "voter1"
+
+    def grouping_by_date_test(self):
+        self.driver.get(f'{self.live_server_url}/admin/')
+        self.driver.find_element_by_id('id_username').send_keys("admin")
+        self.driver.find_element_by_id('id_password').send_keys("qwerty",Keys.ENTER)
+
+        self.driver.get(f'{self.live_server_url}/census/admin/')
+        self.driver.find_element(By.ID, "agrupar_fecha").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#headingFour > .accordion-button").click()
+        self.driver.find_element(By.LINK_TEXT, "Jan. 11, 2021").click()
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(1) > td:nth-child(4)").text == "Jan. 11, 2021"
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(2) > td:nth-child(4)").text == "Jan. 11, 2021"
+
+    def filter_by_test(self):
+        self.driver.get(f'{self.live_server_url}/admin/')
+        self.driver.find_element_by_id('id_username').send_keys("admin")
+        self.driver.find_element_by_id('id_password').send_keys("qwerty",Keys.ENTER)
+
+        self.driver.get(f'{self.live_server_url}/census/admin/')
+        self.driver.find_element(By.ID, "q").click()
+        self.driver.find_element(By.ID, "q").send_keys("Colegio1")
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(1) > td:nth-child(3)").text == "Colegio1"
+        assert self.driver.find_element(By.CSS_SELECTOR, ".filas-tabla:nth-child(2) > td:nth-child(3)").text == "Colegio1"
