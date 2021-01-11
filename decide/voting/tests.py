1f3477b276203e74ac9831227693b809da0e1ecd
Amine Chaghir Chikhaoui <zopizopizala@hotmail.es>
2021-01-11 14:18:50 +0100

    (BUG Escanios)Un encabezado
    Cuerpo del commit
    Se arregla el bug con los escanios

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -37,7 +37,7 @@ class VotingTestCase(BaseTestCase):
         for i in range(5):
             opt = QuestionOption(question=q, option='option {}'.format(i+1))
             opt.save()
-        v = Voting(name='test voting', question=q)
+        v = Voting(name='test voting', question=q, escanios=0)
         v.save()
 
         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
