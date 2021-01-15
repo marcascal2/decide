0962bb49b0c79c752e21b643f76ff1014833ba46
emicolalc <emicolalc@alum.us.es>
2021-01-15 21:12:06 +0100

    (Mejora)Cambio para que no se muestre el navegador
    Modifico los test de selenim para que no muestre el navegador

--- decide/census/UI_tests/grouping_by_voting_test.py
+++ decide/census/UI_tests/grouping_by_voting_test.py
@@ -20,7 +20,7 @@ class TestGroupingbyvotingtest(StaticLiveServerTestCase):
     self.base = BaseTestCase()
     self.base.setUp()
     options = webdriver.ChromeOptions()
-    options.headless = False
+    options.headless = True
     self.driver = webdriver.Chrome(options=options)  
 
     question = Question(desc='desc')
